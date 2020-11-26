362850baf7ba1c6342374780092a4ee3e87e778f
Emaisty <maxgusevfff@yandex.ru>
2020-11-26 10:25:18 +0300

    after program start, it remembers number of messages. Ones per minute, it checks number of messages. If it changes - try find there a link to webinar. Also add stop list of links that have already been

--- main.py
+++ main.py
@@ -4,19 +4,25 @@ import time
 import pyautogui
 from global_var import x_pos_text, y_pos_text, x_pos_button, y_pos_button, name
 
+stop_list = []
+
 
 def act(url):
-    print(url)
-    webbrowser.open(url)
-    time.sleep(5)
-    pyautogui.click(x_pos_text, y_pos_text)
-    pyautogui.click(x_pos_text, y_pos_text)
-    pyautogui.click(x_pos_text, y_pos_text)
-    time.sleep(2)
-    pyautogui.typewrite(name)
-    time.sleep(2)
-    pyautogui.click(x_pos_button, y_pos_button)
-    time.sleep(2)
+    if url in stop_list:
+        print('It\'s already had been')
+    else:
+        stop_list.append(url)
+        print(url)
+        webbrowser.open(url)
+        time.sleep(5)
+        pyautogui.click(x_pos_text, y_pos_text)
+        pyautogui.click(x_pos_text, y_pos_text)
+        pyautogui.click(x_pos_text, y_pos_text)
+        time.sleep(2)
+        pyautogui.typewrite(name)
+        time.sleep(2)
+        pyautogui.click(x_pos_button, y_pos_button)
+        time.sleep(2)
 
 
 def check(str="nope"):
@@ -29,13 +35,14 @@ def check(str="nope"):
 
 
 def find_url(mess):
-    print(1)
+    print(mess)
     if mess['text'].find("events.webinar.ru") + 1:
         print(mess['text'])
         act(check(mess['text']))
     if mess['attachments']:
-        if mess['attachments'][0]['link']['url'].find("events.webinar.ru") + 1:
-            act(check(mess['attachments'][0]['link']['url']))
+        if 'link' in mess['attachments'][0]:
+            if mess['attachments'][0]['link']['url'].find("events.webinar.ru") + 1:
+                act(check(mess['attachments'][0]['link']['url']))
     if 'fwd_messages' in mess:
         if mess['fwd_messages']:
             m = mess['fwd_messages'][0]
@@ -43,7 +50,12 @@ def find_url(mess):
 
 
 if __name__ == "__main__":
+    messages = check_message()
+    num = messages['count']
     while True:
-        messages = check_message()['items'][0]
-        find_url(messages)
-        time.sleep(2)
+        messages = check_message()
+        if num < messages['count']:
+            for i in range(0, messages['count'] - num):
+                find_url(messages['items'][i])
+            num = messages['count']
+        time.sleep(60)
